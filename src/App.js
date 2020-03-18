566962fff384e15e57bd2c1bc59be90b2e173f1b
Enkhtaivan.S <enkhtaivan.s@itc.gov.mn>
2020-03-18 19:59:44 +0800

    Robot haidag app Duuslaa

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
 import { CardList } from "./components/card-list"; // тухайн компонэнтод байгаа .jsx фөйлын нэр нь index бол энэ комп - ийг дуудахдаа файлын нэрийг бичиж өгөх албагүй
+import { SearchBox } from "./components/search-box";
 import "./App.css";
 
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: []
+      robots: [],
+      searchField: ""
     };
+    // react developer - ийн хувьд хамгийн чухал ажил бол энэхүү state төлвүүдийг аль компонэнтод хадгалах вэ гэдгийг боддож олох явдал юм.
   }
+  onSearchChanged = event => {
+    this.setState({ searchField: event.target.value });
+    // console.log(event.target.value);
+  };
 
   // render() - ийн дараа ажилдаг тусгай функц
   componentDidMount() {
@@ -18,11 +25,16 @@ export default class App extends Component {
   }
 
   render() {
-    console.log(this.state.robots);
+    // console.log(this.state.robots);
+    const { robots, searchField } = this.state;
+    const filteredRobots = robots.filter(el =>
+      el.name.toLowerCase().includes(searchField)
+    );
     return (
       <div className="App">
         <h1>Хайлтууд</h1>
-        <CardList robots={this.state.robots} />
+        <SearchBox onSearch={this.onSearchChanged} />
+        <CardList robots={filteredRobots} />
       </div>
     );
   }
